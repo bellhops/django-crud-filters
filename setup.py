a8da4ea773f333a17a765e8f9fbb3ee743096da1
Forrest Pruitt <fpruitt0922@gmail.com>
2015-08-07 14:55:28 -0400

    bumping again...

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='crud_filters',
     # packages=['crud_filters'],
     packages=find_packages(exclude=['tests*']),
-    version='0.1.1',
+    version='0.1.2',
     description='django-crud-filters works with django-restframework to provide easily configurable role-based filtering for API endpoints.',
     author='Bellhops',
     author_email='tech@getbellhops.com',
@@ -16,5 +16,5 @@ setup(
     download_url='https://github.com/bellhops/django-crud-filters/dist/crud_filters-0.1.0.tar.gz',
     keywords=['django', 'authorization', 'api', 'security'],
     classifiers=[],
-    install_requires=['Django', 'djangorestframework', 'djangorestframework-expiring-authtoken'],
+    install_requires=['Django', 'djangorestframework', 'djangorestframework-expiring-authtoken', 'django-dynamic-fixture', 'Pillow'],
 )
