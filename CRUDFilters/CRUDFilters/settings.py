1ed1a3dbc5ee678df47d44514c354d8eee5109e9
Reed Tomlinson <dev1@carbonobjects.com>
2015-08-10 21:05:24 -0400

    Added template loader

--- CRUDFilters/CRUDFilters/settings.py
+++ CRUDFilters/CRUDFilters/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'CRUDFilters.tests.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -74,6 +75,12 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+            ],
+            'loaders': [
+                ('django.template.loaders.cached.Loader', [
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+                ]),
             ],
         },
     },
